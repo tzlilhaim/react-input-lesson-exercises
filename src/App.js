944d762e4e3a4c200aff93423f34ca408510e50d
Tal Bronfer <tal.bronfer@safebreach.com>
2018-10-11 12:51:14 +0300

    test maintenance

--- src/App.js
+++ src/App.js
@@ -1,60 +1,19 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      age: '',
-      name: '',
-      fruit: ''
-    }
+    this.state = {};
   }
   render() {
     return (
       <div>
-        <input placeholder="name please" value={this.state.name} className="name-input" onChange={this.updateName}/>
-        <select className="option-menu" onChange={this.updateFruit} value={this.state.fruit}>
-          <option value="apple">Apple</option>
-          <option value="orange">Orange</option>
-          <option value="peach">Peach</option>
-        </select>
+        <input type="text" />
+        <button>Alert</button>
       </div>
     )
   }
-
-  showAlert = () => {
-    if (this.state.age >= 16 ) {
-      alert("welcome to the club, " + this.state.name);
-    }
-    else {
-      alert("You are too young to enter");
-    }
-  }
-
-  changeText = () => { //don't forget arrow syntax for correct `this` binding
-    this.setState({
-      testText: "Something else"
-    });
-}
-
-updateAge = (event) => {
-  this.setState({ age: event.target.value
-  })
-};
-
-updateName = (event) => {
-  this.setState({ name: event.target.value });
-};
-
-updateFruit = (event) => {
-  this.setState({ fruit: event.target.value}, () => {
-    console.log(this.state.name + " selected " + this.state.fruit);
-  });
-  
-}
-alertText = () => alert(this.state.testText)
-
 }
 
 export default App;
