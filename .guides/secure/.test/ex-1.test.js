2f0f522eb38321ef9f2133ef12dc59221bdc41bb
Tal Bronfer <tal.bronfer@safebreach.com>
2018-10-13 22:05:50 +0300

    fixed path

--- .guides/secure/.test/ex-1.test.js
+++ .guides/secure/.test/ex-1.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../../src/App';
+import App from '../../../src/App';
 import { mount, render, shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { wrap } from 'module';
