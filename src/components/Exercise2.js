4670a0fcc1d84fbfe0a2da5e53d25c9e2a48eaa8
jona <jona@elevation.ac>
2019-04-30 18:59:31 +0300

    ex 2 add [ci skip]

--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 
 class Exercise2 extends Component {
 
-    render() {
-        return (
-            <div>Feed me code.</div>
-        );
+constructor() {
+    super()
+    this.state = {
+        name: "",
+        fruit: ""
     }
+}
+
+render() {
+    return (
+        <div>
+            <input id="name-input" />
+            <select id="select-input"></select>
+        </div>
+    );
+}
 }
 
 export default Exercise2;
