c08d34493753de710d87000fc1b2a815c9fc996d
jona <jona@elevation.ac>
2019-04-30 18:36:12 +0300

    ex2

--- test/exercises/ex-2.test.js
+++ test/exercises/ex-2.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../../src/App';
-import { mount, render, shallow, configure} from 'enzyme';
+import { mount, render, shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { wrap } from 'module';
+import Exercise2 from '../../src/components/Exercise2';
 
 configure({ adapter: new Adapter() });
 
@@ -14,14 +14,15 @@ it('Application should render without crashing', () => {
 });
 
 it('There should be a `fruit` property in state that is updated when changing selection in the dropdown with the class `option-menu`', () => {
-    const wrapper = mount(<App />);
-    // get the last fruit option
-    let options = wrapper.find('option'); 
-    let optionValue = options.at(1).prop('value') || options.at(1).text();
-    
-    let select = wrapper.find('.option-menu')
-    
-    select.instance().value = optionValue
-    select.simulate('change')
-    expect(wrapper.state("fruit")).toEqual(optionValue);
+  const wrapper = mount(<Exercise2 />)
+
+  let options = wrapper.find('option');
+  expect(options.length, "There should be at least 1 <option> inside the select").toBeGreaterThanOrEqual(1)
+
+  let optionValue = options.at(0).prop('value') || options.at(0).text();
+  let select = wrapper.find('#select-input')
+
+  select.instance().value = optionValue
+  select.simulate('change')
+  expect(wrapper.state("fruit"), "When a different drop-down value is selected, the state's `fruit` property should change to the selected option").toEqual(optionValue);
 });
