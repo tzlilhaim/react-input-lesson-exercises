f2d56ff22110811c1d6de764c1f7b1092fb62d11
tzlilhaim <tzlilhaim480@gmail.com>
2020-10-05 11:22:22 +0300

    Added solution to exercise 2

--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -1,23 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
 class Exercise2 extends Component {
-
-constructor() {
+  constructor() {
     super()
     this.state = {
-        name: "",
-        fruit: ""
+      name: "",
+      fruit: "",
     }
-}
+  }
+  handleInputsChange = (event) => {
+    const target = event.target
+    const value = target.value
+    let name = target.id.split("-")[0]
+    name = name === "select" ? "fruit" : name
 
-render() {
+    this.state.fruit.length
+      ? null
+      : this.setState({ fruit: document.getElementById("select-input").value })
+    this.setState({ [name]: value }, () => {
+      console.log(`${this.state.name} selected ${this.state.fruit}`)
+    })
+  }
+
+  render() {
     return (
-        <div>
-            <input id="name-input" />
-            <select id="select-input"></select>
-        </div>
-    );
-}
+      <div>
+        <input
+          id="name-input"
+          value={this.state.name}
+          onChange={this.handleInputsChange}
+        />
+        <select
+          id="select-input"
+          value={this.state.fruit}
+          onChange={this.handleInputsChange}
+        >
+          <option>Apple</option>
+          <option>Orange</option>
+          <option>Banana</option>
+          <option>Pear</option>
+        </select>
+      </div>
+    )
+  }
 }
 
-export default Exercise2;
+export default Exercise2
